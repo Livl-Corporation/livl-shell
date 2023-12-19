84a14d758975352b32fe5b089ca9bebd1b8291b9
Julien Von Der Marck <julien.von-der-marck@etu.unistra.fr>
2023-12-19 20:34:54 +0100

    fix(main): handle when there is only one length of operator

--- src/main.c
+++ src/main.c
@@ -1,26 +1,68 @@
 #include "main.h"
 
-#include "main.h"
-
 void preprocess_input(char* input) {
     char* operators[] = {"&&", "||", ">", "<", "&", ";"};
     int num_operators = sizeof(operators) / sizeof(char*);
 
     for (int i = 0; i < num_operators; i++) {
         char* pos = input;
+        // Replace all operators with a space before and after
         while ((pos = strstr(pos, operators[i])) != NULL) {
-            memmove(pos + 1, pos, strlen(pos) + 1);
-            *pos = ' ';
-            pos += 3;
-            memmove(pos + 1, pos, strlen(pos) + 1);
-            *pos = ' ';
+            memmove(pos + strlen(operators[i]), pos, strlen(pos) + 1);
+            for (int j = 0; j < strlen(operators[i]); j++) {
+                pos[j] = ' ';
+            }
+            pos += strlen(operators[i]) + 1;
+        }
+    }
+
+    // Handle single character operators separately
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] == '>' || input[i] == '<' || input[i] == '&' || input[i] == ';') {
+            memmove(&input[i+1], &input[i], strlen(input) - i + 1);
+            input[i] = ' ';
+            input[i+2] = ' ';
+            i += 2;
         }
     }
 }
 
 
-int main() {
+int is_operator(char *token) {
+    char* operators[] = {"&&", "||", ">", "<", "&", ";"};
+    int num_operators = sizeof(operators) / sizeof(char*);
 
+    for (int i = 0; i < num_operators; i++) {
+        if (strcmp(token, operators[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void process_token(char *token, char **commands, char **operators, char **currentCommand, int *cmdIndex, int *opIndex) {
+    if (is_operator(token)) {
+        operators[(*opIndex)++] = strdup(token);
+    } else {
+        if (*currentCommand == NULL) {
+            *currentCommand = strdup(token);
+        } else {
+            size_t len = strlen(*currentCommand) + strlen(token) + 2;
+            *currentCommand = realloc(*currentCommand, len);
+            strcat(*currentCommand, " ");
+            strcat(*currentCommand, token);
+        }
+    }
+}
+
+void store_command(char **commands, char **currentCommand, int *cmdIndex) {
+    if (*currentCommand != NULL) {
+        commands[(*cmdIndex)++] = *currentCommand;
+        *currentCommand = NULL;
+    }
+}
+
+int main() {
     print_title();
 
     char input[MAX_INPUT_LENGTH];
@@ -31,10 +73,8 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    // Preprocess the input
     preprocess_input(input);
 
-    // Remove newline character if present
     size_t len = strlen(input);
     if (len > 0 && input[len - 1] == '\n') {
         input[len - 1] = '\0';
@@ -44,7 +84,6 @@ int main() {
     char *commands[MAX_COMMANDS];
     char *operators[MAX_COMMANDS - 1];
 
-    // Tokenize the input based on spaces
     token = strtok(input, " \n");
 
     int cmdIndex = 0;
@@ -57,36 +96,15 @@ int main() {
     sequence.num_commands = 0;
 
     while (token != NULL) {
-        if (strcmp(token, "&&") == 0 || strcmp(token, "||") == 0 || strcmp(token, ">") == 0 ||
-            strcmp(token, "<") == 0 || strcmp(token, "&") == 0 || strcmp(token, ";") == 0) {
-            // It's an operator
-            operators[opIndex++] = strdup(token);
-        } else {
-            // It's part of a command
-            if (currentCommand == NULL) {
-                currentCommand = strdup(token);
-            } else {
-                size_t len = strlen(currentCommand) + strlen(token) + 2;
-                currentCommand = realloc(currentCommand, len);
-                strcat(currentCommand, " ");
-                strcat(currentCommand, token);
-            }
-        }
+        process_token(token, commands, operators, &currentCommand, &cmdIndex, &opIndex);
 
-        // Move to the next token
         token = strtok(NULL, " \n");
 
-        // If an operator is encountered or we reach the end, store the current command
-        if (token == NULL || strcmp(token, "&&") == 0 || strcmp(token, "||") == 0 ||
-            strcmp(token, ">") == 0 || strcmp(token, "<") == 0 || strcmp(token, "&") == 0 || strcmp(token, ";") == 0) {
-            if (currentCommand != NULL) {
-                commands[cmdIndex++] = currentCommand;
-                currentCommand = NULL;
-            }
+        if (token == NULL || is_operator(token)) {
+            store_command(commands, &currentCommand, &cmdIndex);
         }
     }
 
-    // Populate the CommandSequence structure
     for (int i = 0; i < cmdIndex; i++) {
         sequence.commands[sequence.num_commands++] = evaluateCommand(commands[i]);
         free(commands[i]);
@@ -94,17 +112,16 @@ int main() {
 
     for (int i = 0; i < opIndex; i++) {
         sequence.operators[i] = strdup(operators[i]);
+        printf(" * Operator: %s\n", sequence.operators[i]);
         free(operators[i]);
     }
 
-    // Execute the command sequence
     int result = executeCommandSequence(&sequence);
 
     if (result < 0) {
         fprintf(stderr, "An error occurred while executing the commands\n");
     }
 
-    // Free the memory
     freeCommandSequence(&sequence);
 
     return EXIT_SUCCESS;
