fa15941ad8836be8364b1f664cd1c8d961a0720a
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-01-02 11:53:21 +0100

    Revert "fix(history): remove implementation of arrows keys"
    This reverts commit f1205af23cf86ef7f1d3a0c9f79b2ebdaca2cc22.

--- src/input_parser.c
+++ src/input_parser.c
@@ -1,22 +1,63 @@
 #include "input_parser.h"
 
-char* read_input(char *input) {
+char *read_input(char *input)
+{
     if (input == NULL)
     {
         perror("input is not allocated");
         return NULL;
     }
 
-    if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
-        perror("fgets");
-        free(input);
-        return NULL;
-    }
+    struct termios old_termios, new_termios;
+    tcgetattr(STDIN_FILENO, &old_termios);
+    new_termios = old_termios;
+    new_termios.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
 
-    size_t len = strlen(input);
-    if (len > 0 && input[len - 1] == '\n') {
-        input[len - 1] = '\0';
+    int index = 0;
+    int c;
+    while ((c = getchar()) != '\n')
+    {
+        if (c == 27) // Escape character
+        {
+            getchar(); // Skip the [
+            switch (getchar())
+            {
+            case 'A': // Arrow up
+                handle_arrow_up(input, &index);
+                break;
+            case 'B': // Arrow down
+                handle_arrow_down(input, &index);
+                break;
+            case 'D': // Arrow left
+                handle_arrow_left(input, &index);
+                break;
+            case 'C': // Arrow right
+                handle_arrow_right(input, &index);
+                break;
+            default:
+                break;
+            }
+        }
+        else if (c == 127) // Backspace
+        {
+            handle_backspace(input, &index);
+        }
+        else if (c == 8) // Control (or command on macos) + backspace
+        {
+            handle_control_backspace(input, &index);
+        }
+        else if (isalnum(c) || isspace(c) || ispunct(c)) // Check if the character is alphanumeric, whitespace, or punctuation
+        {
+            printf("%c", c);
+            input[index++] = c;
+        }
     }
+    input[strlen(input)] = '\0';
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
+    printf("\n");
+    reset_current_index();
 
     return input;
 }
@@ -73,4 +114,73 @@ void preprocess_input(char *input)
     new_input[j] = '\0';
     strcpy(input, new_input);
     free(new_input);
+}
+
+void handle_arrow_up(char *input, int *index)
+{
+    char command[MAX_INPUT_LENGTH];
+    get_previous_command(command);
+    replace_input(input, index, command);
+}
+
+void handle_arrow_down(char *input, int *index)
+{
+    char command[MAX_INPUT_LENGTH];
+    get_next_command(command);
+    replace_input(input, index, command);
+}
+
+void handle_arrow_left(char *input, int *index)
+{
+    if (*index > 0)
+    {
+        printf("\b");
+        (*index)--;
+    }
+}
+
+void handle_arrow_right(char *input, int *index)
+{
+    if (*index < strlen(input))
+    {
+        printf("%c", input[*index]);
+        (*index)++;
+    }
+}
+
+void handle_backspace(char *input, int *index)
+{
+    if (*index == 0)
+    {
+        return;
+    }
+
+    // Shift characters to the left
+    for (int i = *index - 1; i < strlen(input); i++)
+    {
+        input[i] = input[i + 1];
+    }
+
+    // Update index
+    (*index)--;
+
+    // Print the new input
+    printf("\33[2K\r");
+    print_prompt();
+    printf("%s", input);
+    printf("\33[%dD", (int)strlen(input) - *index);
+}
+
+void handle_control_backspace(char *input, int *index)
+{
+    replace_input(input, index, "");
+}
+
+void replace_input(char *input, int *index, const char *replacement)
+{
+    printf("\33[2K\r");
+    print_prompt();
+    printf("%s", replacement);
+    strcpy(input, replacement);
+    *index = strlen(replacement);
 }
